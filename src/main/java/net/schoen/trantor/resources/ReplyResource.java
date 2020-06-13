eb8ff3c4e4215e3956c42105e7187cd3e54ab0ec
russell schoen <rschoen@fastmail.fm>
2020-06-13 08:06:26 -0500

    Attempted to add push/delete endpoints

--- src/main/java/net/schoen/trantor/resources/ReplyResource.java
+++ src/main/java/net/schoen/trantor/resources/ReplyResource.java
@@ -4,41 +4,52 @@ import net.schoen.trantor.entities.ReplyEntity;
 import net.schoen.trantor.repositories.ReplyRepository;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/api/replies")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ReplyResource {
     @Inject
     ReplyRepository replyRepository;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<ReplyEntity> all() {
         return replyRepository.all();
     }
 
     @GET
     @Path("/reply-id/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public ReplyEntity findById(@PathParam("id") Integer id) {
         return replyRepository.findById(id);
     }
 
+    @PUT
+    @Path("/reply-id/{id}")
+    @Transactional
+    public void putById(@PathParam("id") Integer id, ReplyEntity reply) {
+        replyRepository.persist(reply);
+    }
+
+    @DELETE
+    @Path("reply-id/{id}")
+    @Transactional
+    public void deleteById(@PathParam("id") Integer id) {
+        ReplyEntity r = replyRepository.findById(id);
+        replyRepository.delete(r);
+    }
+
     @GET
     @Path("/thread-id/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public List<ReplyEntity> findByThread(@PathParam("id") Integer id) {
         return replyRepository.findByThread(id);
     }
 
     @GET
     @Path("/user-id/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public List<ReplyEntity> findByUser(@PathParam("id") Integer id) {
         return replyRepository.findByUser(id);
     }
--- src/main/java/net/schoen/trantor/resources/ThreadResource.java
+++ src/main/java/net/schoen/trantor/resources/ThreadResource.java
@@ -4,27 +4,24 @@ import net.schoen.trantor.repositories.ThreadRepository;
 import net.schoen.trantor.entities.ThreadEntity;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/api/threads")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class ThreadResource {
 
     @Inject
     ThreadRepository threadRepository;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<ThreadEntity> getAll() {
         return threadRepository.all();
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     @Path("/{id}")
     public ThreadEntity getById(@PathParam("id") Integer id) {
         return threadRepository.findById(id);
